d992cf0cebd907211198709ef9554ee47010ea18
danerickperalta <danerick@humainco.com>
2025-05-12 20:21:12 -0400

    Target UI Color Updates

--- code.ts
+++ code.ts
@@ -1,3 +1,13 @@
+function rgbToHex(color: RGB): string {
+  const r = Math.round(color.r * 255);
+  const g = Math.round(color.g * 255);
+  const b = Math.round(color.b * 255);
+  return `#${((1 << 24) + (r << 16) + (g << 8) + b)
+    .toString(16)
+    .slice(1)
+    .toUpperCase()}`;
+}
+
 figma.showUI(__html__, { width: 360, height: 300 });
 
 figma.ui.onmessage = (msg) => {
